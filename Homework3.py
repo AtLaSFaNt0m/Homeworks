761c3fe9d9975d970ac581a0836988fd027a4b27
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-04-11 23:06:34 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,7 @@
+my_string = input('Введите предложение: ')
+print(my_string + '. Длина предложения: ', len(my_string))
+print(my_string.upper() + '. В верхнем регистре ".upper()"')
+print(my_string.lower() + '. В нижнем регистре ".lower()"')
+print(my_string.replace(' ', '') + '. Без пробелов ".replace()"')
+print(my_string[0] + '. Первый символ предложения')
+print(my_string[-1] + '. Последний символ предложения')
