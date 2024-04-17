f2b8797d271b065134ec115d895dfefd6b57a7ae
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-04-17 20:11:16 +0300

    Update HomeWork9.py

--- HomeWork9.py
+++ HomeWork9.py
@@ -1,6 +1,6 @@
 import random
 
-date_month = (1, 2, 3,
+date_month = [1, 2, 3,
               4, 5, 6,
               7, 8, 9,
               10, 11, 12,
@@ -9,7 +9,7 @@ date_month = (1, 2, 3,
               19, 20, 21,
               22, 23, 24,
               25, 26, 27,
-              28, 29, 30)
+              28, 29, 30]
 weather = ['Sunny', 'Rainy', 'Storm',
            'Thunder', 'Snow', 'Snowfall',
            'Cloudy', 'Hail', 'Eclipse']
@@ -18,8 +18,10 @@ weather = ['Sunny', 'Rainy', 'Storm',
 def print_params(date_month, weather):
     day = random.choice(date_month)
     weather_day = random.choice(weather)
+    print(date_month)
+    print(weather)
     return day, weather_day
 
 
-day, weather_day = print_params(date_month, weather)
-print(f'Today: {day} weather: {weather_day}')
+print_params(date_month, weather)
+print_params(date_month, weather)
