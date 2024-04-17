2f48a001e7eb674655e590de497242773f1a3ae6
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-04-17 17:46:31 +0300

    Add files via upload

---
+++ HomeWork8.py
@@ -0,0 +1,6 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in range(cars.__len__()):
+    cars_count += 10
+    print(f'Я езжу на автомабиле марки {cars[i]}')
+    print(f'cars_count {cars_count}')
