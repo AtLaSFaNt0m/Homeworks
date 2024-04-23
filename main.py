99b306c19b9210836c0f7989d377b2abe3a7d67b
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-04-23 17:38:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+def test():
+    a = 10
+    b = 20
+    print("Значение переменной a:", a)
+    print("Значение переменной b:", b)
+
+
+test()
+
+
+def test2(param1, param2, param3):
+    print("Значение параметра 1:", param1)
+    print("Значение параметра 2:", param2)
+    print("Значение параметра 3:", param3)
+
+
+test2(5, "Hello", True)
