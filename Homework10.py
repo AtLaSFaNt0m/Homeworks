b21a08051d8e11c03ab271deb35f0ff774d8562d
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-04-29 00:34:19 +0300

    Add files via upload

---
+++ Homework10.py
@@ -0,0 +1,14 @@
+def print_params(a=1, b='String', c=True):
+    print(a, b, c)
+
+
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+
+value_list = [4, 2, 3]
+values_dict = {'a': 132, 'b': 'String', 'c': True}
+
+
+print_params(*value_list)
+print_params(**values_dict)
