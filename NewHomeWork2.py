5e25d20cc42894235cb4f7387add2d73d3b532a6
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-05-30 20:45:01 +0300

    Add files via upload

---
+++ NewHomeWork2.py
@@ -0,0 +1,5 @@
+Homework = 12
+hours = 1.5
+language = 'Python'
+Hours_on_one_work = hours / Homework
+print(f'Курс:{language}, всего задач:{Homework}, затрачено часов: {hours}, среднее время выполнения {Hours_on_one_work}.')
