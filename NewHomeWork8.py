368f743a4f048a8b865fac9fb07e2efb85a27bfd
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-05-30 23:17:52 +0300

    Add files via upload

---
+++ NewHomeWork8.py
@@ -0,0 +1,12 @@
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+
+index = 0
+
+while index < len(my_list):
+    if my_list[index] < 0:
+        break
+    print(my_list[index])
+    index += 1
+
