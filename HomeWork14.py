e1b4c2e81f060980c89e84b0e31bcb60bd7eed4c
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-06-11 17:37:53 +0300

    HomeWork14.py

--- HomeWork14.py
+++ HomeWork14.py
@@ -1,21 +1,26 @@
-class Vehicle:
-    vehicle_type = "none"
-
 class Car:
     price = 1000000
 
     def horse_powers(self):
         return "Количество лошадиных сил для автомобиля не указано"
 
-class Nissan(Car, Vehicle):
+class Nissan(Car):
     price = 1200000
-    vehicle_type = "car"
 
     def horse_powers(self):
         return 200
 
+class Kia(Car):
+    price = 900000
+
+    def horse_powers(self):
+        return 150
+
 
 nissan_car = Nissan()
+kia_car = Kia()
 
-print(nissan_car.vehicle_type)
-print(nissan_car.price)
+print(f"Nissan: vehicle_type = 'car', price = {nissan_car.price}, horse_powers = {nissan_car.horse_powers()}")
+
+
+print(f"Kia: vehicle_type = 'car', price = {kia_car.price}, horse_powers = {kia_car.horse_powers()}")
