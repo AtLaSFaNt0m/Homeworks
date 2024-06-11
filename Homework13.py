be9c72609c2667fd121189b730d94bdfcfa07e8a
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-06-11 17:05:57 +0300

    Add files via upload

---
+++ Homework13.py
@@ -0,0 +1,18 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 1 > new_floor or new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+
+
+house = House('ЖК Эльбрус', 30)
+
+
+house.go_to(5)
+house.go_to(35)
