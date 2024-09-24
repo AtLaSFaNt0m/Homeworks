b14eca83d47e968fd0602c190daa3f361604132a
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-09-25 01:37:07 +0300

    Add files via upload

---
+++ Homework29.py
@@ -0,0 +1,10 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(s) for s in first_strings if len(s) >= 5]
+second_result = [(s1, s2) for s1 in first_strings for s2 in second_strings if len(s1) == len(s2)]
+third_result = {s: len(s) for s in first_strings + second_strings if len(s) % 2 == 0}
+
+print(first_result)
+print(second_result)
+print(third_result)
