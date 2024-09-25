5fa56b550425d64237410f9dee69dad518328ff2
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-09-25 18:52:45 +0300

    Add files via upload

---
+++ Homework37.py
@@ -0,0 +1,34 @@
+import threading
+import time
+
+class Knight(threading.Thread):
+    def __init__(self, name, power):
+        super().__init__()
+        self.name = name
+        self.power = power
+        self.enemies = 100
+        self.days = 0
+
+    def run(self):
+        print(f"{self.name}, на нас напали!")
+        while self.enemies > 0:
+            time.sleep(1)
+            self.days += 1
+            self.enemies -= self.power
+            self.enemies = max(0, self.enemies)
+            print(f"{self.name}, сражается {self.days} день(дня)..., осталось {self.enemies} воинов.")
+
+        print(f"{self.name} одержал победу спустя {self.days} дней(дня)!")
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+
+first_knight.start()
+second_knight.start()
+
+
+first_knight.join()
+second_knight.join()
+
+
+print("Все битвы закончились!")
