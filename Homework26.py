e9e42275fbbf3429fd374704a75a94ca9ea60856
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-09-25 16:12:19 +0300

    Homework26.py

--- Homework26.py
+++ Homework26.py
@@ -6,30 +6,24 @@ def personal_sum(numbers):
         try:
             result += item
         except TypeError:
+            print(f"Некорректный тип данных для подсчёта суммы - {item}")
             incorrect_data += 1
-            print(f'Некорректный тип данных для подсчёта суммы - {item}')
 
     return result, incorrect_data
 
 
 def calculate_average(numbers):
     try:
-        if not isinstance(numbers, (list, tuple, set)):
-            raise TypeError
-
         total_sum, incorrect_data = personal_sum(numbers)
-
-        valid_count = len(numbers) - incorrect_data
-        if valid_count == 0:
-            raise ZeroDivisionError
-
-        return total_sum / valid_count
-
-    except ZeroDivisionError:
-        return 0
+        count = len(numbers) - incorrect_data
+        if count == 0:
+            return 0
+        return total_sum / count
     except TypeError:
         print('В numbers записан некорректный тип данных')
         return None
+    except ZeroDivisionError:
+        return 0
 
 
 print(f'Результат 1: {calculate_average("1, 2, 3")}')
