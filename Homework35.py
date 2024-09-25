1ffe8036269c2202e52d5a38510f67dace555216
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-09-25 16:17:18 +0300

    Add files via upload

---
+++ Homework35.py
@@ -0,0 +1,21 @@
+def is_prime(func):
+    def wrapper(a, b, c):
+        result = func(a, b, c)
+        if result < 2:
+            print("Составное")
+        else:
+            for i in range(2, int(result ** 0.5) + 1):
+                if result % i == 0:
+                    print("Составное")
+                    break
+            else:
+                print("Простое")
+        return result
+    return wrapper
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+result = sum_three(2, 3, 6)
+print(result)
