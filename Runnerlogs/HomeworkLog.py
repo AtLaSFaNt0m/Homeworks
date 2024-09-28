41821f149cb5037566feb363a1afe7b173d62d04
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-09-28 18:59:59 +0300

    HomeworkLog.py

--- Runnerlogs/HomeworkLog.py
+++ Runnerlogs/HomeworkLog.py
@@ -1,6 +1,6 @@
 import unittest
 import logging
-from Homework48 import Runner  # Предположим, что класс Runner находится в файле runner.py
+from Homework48 import Runner
 
 # Настройка логирования
 logging.basicConfig(
@@ -15,22 +15,20 @@ class RunnerTest(unittest.TestCase):
 
     def test_walk(self):
         try:
-            # Попытка создать объект Runner с отрицательной скоростью
             runner = Runner(name="Test Runner", speed=-5)
             for _ in range(10):
                 runner.walk()
-            self.assertEqual(runner.distance, 50)  # Это не должно быть выполнено
+            self.assertEqual(runner.distance, 50)
             logging.info('"test_walk" выполнен успешно')
         except ValueError as e:
             logging.warning("Неверная скорость для Runner: %s", e)
 
     def test_run(self):
         try:
-            # Попытка создать объект Runner с неверным типом для имени
             runner = Runner(name=123, speed=5)
             for _ in range(10):
                 runner.run()
-            self.assertEqual(runner.distance, 100)  # Это не должно быть выполнено
+            self.assertEqual(runner.distance, 100)
             logging.info('"test_run" выполнен успешно')
         except TypeError as e:
             logging.warning("Неверный тип данных для объекта Runner: %s", e)
