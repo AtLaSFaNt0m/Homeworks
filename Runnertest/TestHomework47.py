3e064afb0ba26aecd247f0cc34c96a2f59b99ad2
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-09-28 18:35:20 +0300

    TestHomework47.py

--- Runnertest/TestHomework47.py
+++ Runnertest/TestHomework47.py
@@ -2,11 +2,9 @@ import unittest
 from Homework47 import RunnerTest, TournamentTest
 
 
-# Создание TestSuite
 def suite():
     suite = unittest.TestSuite()
 
-    # Загрузка тестов из классов RunnerTest и TournamentTest
     loader = unittest.defaultTestLoader
     suite.addTests(loader.loadTestsFromTestCase(RunnerTest))
     suite.addTests(loader.loadTestsFromTestCase(TournamentTest))
@@ -15,6 +13,6 @@ def suite():
 
 
 if __name__ == '__main__':
-    # Запуск тестов
+
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite())
