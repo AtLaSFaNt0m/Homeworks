6e3fa1bdd98740538528302cc705437c13e7baa0
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-09-30 03:16:59 +0300

    Add files via upload

---
+++ Homework62.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+users = []
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
+@app.get("/users", response_model=List[User])
+async def get_users():
+    return users
+
+@app.post("/user/{username}/{age}", response_model=User)
+async def create_user(username: str, age: int):
+    if len(users) == 0:
+        user_id = 1
+    else:
+        user_id = users[-1].id + 1
+    new_user = User(id=user_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
+
+@app.put("/user/{user_id}/{username}/{age}", response_model=User)
+async def update_user(user_id: int, username: str, age: int):
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
+
+@app.delete("/user/{user_id}", response_model=User)
+async def delete_user(user_id: int):
+    for user in users:
+        if user.id == user_id:
+            users.remove(user)
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
