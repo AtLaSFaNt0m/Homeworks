8af81823718290a9da584d3c291c6048b2f5c993
AtLaSFaNt0m <86754506+AtLaSFaNt0m@users.noreply.github.com>
2024-10-18 14:52:12 +0300

    Delete fallout_store directory

--- fallout_store/videogames/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('games/', views.game_list, name='game_list'),
-    path('cart/', views.cart, name='cart'),
-]
